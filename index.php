cff16936d61524071404106d64f47c3aa31ff9ff
Raheel Mansoor <raheelmnsr@gmail.com>
2023-08-14 19:52:23 +0500

    7. 1 july

--- index.php
+++ index.php
@@ -24,10 +24,10 @@
                             <button id ="home_btn"class="nav_btn">Home</button>
                         </li>
                         <li class="items">
-                            <button id= "add_record_btn" class="nav_btn">Products</button>
+                            <button id= "products_btn" class="nav_btn">Products</button>
                         </li>
                         <li class="items">
-                            <button id= "add_record_btn" class="nav_btn">Register</button>
+                            <button id= "register_btn" class="nav_btn">Register</button>
                         </li>
                         <li class="items cart">
                             <a><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a>
@@ -92,7 +92,7 @@
 <script src="http://localhost/ecommerce/js/jquery.js"></script>
 <script>
     $('document').ready(function(){
-        //Populate side navbar
+        //Function to populate side navigation bar with brands and categories records
         function load_sidenav(){
 
             //empty side-nav ul
@@ -104,32 +104,47 @@
                 type: "GET",
                 dataType:"json",
                 success: function(data){
+                    debugger;
+                    //Clear side nav bar
+                    $('#side_nav').html('');
+
+                    //Add brand header 
+                    $('#side_nav').append('<li class="side_nav_header">Brands</li>');
+
+                    //check if brands records exist in database
                     if(data.brands.error){
-                        $('#side_nav').append('<li class="side_nav_h">Brands</li>');
-                        $('#side_nav').append('<li><a class="side_nav_hov" href="">'+data.brands.message+'</a></li>');
+                        $('#side_nav').append('<li class="side_nav_body">'+data.brands.message+'</li>');
                     }else{
-                        $('#side_nav').append('<li class="side_nav_h">Brands</li>');
+                        //Add brands to side navigation bar
                         $.each(data.brands.data,function(key,value){
-                            $('#side_nav').append('<li><a class="side_nav_hov" href="brand'+value.b_id+'">'+value.b_name+'</a></li>');
+                            $('#side_nav').append('<li><input type="button" class="side_nav_btn side_nav_brand_btn"  value="'+value.b_name+'" data-id="'+value.b_id+'"></li>');                                      
                         });                       
                     }
+
+                    //Add category header 
+                    $('#side_nav').append('<li class="side_nav_header">Categories</li>');
+
+                    //check if brands records exist in database
                     if(data.categories.error){
-                        $('#side_nav').append('<li class="side_nav_h">Categories</li>');
-                        $('#side_nav').append('<li><a class="side_nav_hov" href="">'+data.categories.message+'</a></li>');
-                    }else {
-                        $('#side_nav').append('<li class="side_nav_h">Categories</li>');
+                        $('#side_nav').append('<li class="side_nav_body">'+data.categories.message+'</li>');
+                    }else{
+                        //Add Categories to side navigation bar
                         $.each(data.categories.data,function(key,value){
-                            $('#side_nav').append('<li><a class="side_nav_hov" href="category'+value.c_id+'">'+value.c_name+'</a></li>');
-                        });                                              
+                            $('#side_nav').append('<li><input type="button" class="side_nav_btn side_nav_category_btn"  value="'+value.c_name+'" data-id="'+value.c_id+'"></li>');                                      
+                        });                       
                     }
-                }
+
+                 }
 
             });    
             
         }
 
-        function load_cards(card_type){
-            obj = {card_type:card_type};
+        //Function to load cards based on type ie all,brand or category
+        function load_cards(card_type,id){
+            obj = {card_type:card_type,
+                    id:id
+                  };
             json_obj = JSON.stringify(obj);
             $.ajax({               
                 url:"http://localhost/ecommerce/rest_api/api_fetch_products.php",
@@ -138,9 +153,20 @@
                 dataType:"json",
                 success:function(data){
                     debugger;
+
+                    //clear cards
+                    $('#cards_container').html('');
+                    //clear message class
+                    $('#products_msg').removeClass('products_msg_style');
+                    
+
                     //check if no record found
                     if(data.error){
-                        $('#products_msg').addClass('form_msg').text(data.message);
+                        $('#products_msg').fadeIn('slow');
+                        $('#products_msg').addClass('products_msg_style').text(data.message);
+                        setTimeout(function(){
+                            $('#products_msg').fadeOut('slow');
+                        },2000);                           
                     }
                     else{
                         $.each(data.data,function(key, value){
@@ -149,6 +175,7 @@
                                                         '<div class="card_body">'+
                                                             '<h5 class="card_title card_row">'+value.p_title+'</h5>'+
                                                             '<p class="card_text card_row">'+value.p_description+'</p>'+
+                                                            '<p class="card_row card_price"> Price: '+value.p_price+'/-</p>'+
                                                             '<div class="card_row">'+                                        
                                                                 '<button class="card_btn">Add to cart</button>'+
                                                                 '<button class="card_btn">View</button>'+
@@ -163,11 +190,42 @@
             });
         }        
 
+        //load side navigation bar with brands and categories records
         load_sidenav();
-        load_cards('all');
 
+        //load all cards
+        load_cards('all',0);
 
+        //if User press one of the side navigation bar brand button
+        $(document).on('click','.side_nav_brand_btn',function(){
             
+            var brand_id = $(this).data('id');
+            load_cards('brand',brand_id)
+        });
+        
+        //if User press one of the side navigation bar cateogry button
+        $(document).on('click','.side_nav_category_btn',function(){
+            
+            var category_id = $(this).data('id');
+            load_cards('category',category_id)
+        }); 
+        
+        //if user has pressed home button
+        $('#products_btn').on('click',function(){
+            load_cards('all',0);
+        });
+
+        //if user has pressed search button
+        $('#search_btn').on('click',function(){
+            
+            var search = $('#input_search').val();
+            if(search != ""){
+
+                load_cards('search',search);
+            }
+
+        }); 
+                
     });
 </script>
 </html>
--- rest_api/api_fetch_products.php
+++ rest_api/api_fetch_products.php
@@ -11,6 +11,8 @@ include "../include/config.php";
 
 $data = json_decode(file_get_contents("php://input"),true);
 
+    $output = [];
+
     switch($data['card_type']){
         case 'all':
             //sql query to fetch all records
@@ -20,7 +22,55 @@ $data = json_decode(file_get_contents("php://input"),true);
                 $output['data'] = mysqli_fetch_all($result,MYSQLI_ASSOC);
                 $output['error']=false;
                 echo json_encode($output,JSON_PRETTY_PRINT);
-            }else echo json_encode(array('message'=>"No record found", "error"=>true),JSON_PRETTY_PRINT);
+            }else{
+                echo json_encode(array('message'=>"No record found", "error"=>true),JSON_PRETTY_PRINT);
+            }
+            break;
+
+        case 'brand':
+
+            $brand_id = $data['id'];
+
+            //sql query to fetch all records
+            $sql = "SELECT * FROM products WHERE brand_id = '$brand_id'";
+
+            $result = mysqli_query($conn,$sql) or die('Failed to fetch brand records from DB');
+            if(mysqli_num_rows($result)>0){
+                $output['data'] = mysqli_fetch_all($result,MYSQLI_ASSOC);
+                $output['error']=false;
+                echo json_encode($output,JSON_PRETTY_PRINT);
+            }else echo json_encode(array('message'=>"No record found", "error"=>true),JSON_PRETTY_PRINT);  
+            break;
+
+        case 'category':
+
+            $category_id = $data['id'];
+
+            //sql query to fetch all records
+            $sql = "SELECT * FROM products WHERE category_id = '$category_id'";
+
+            $result = mysqli_query($conn,$sql) or die('Failed to fetch brand records from DB');
+            if(mysqli_num_rows($result)>0){
+                $output['data'] = mysqli_fetch_all($result,MYSQLI_ASSOC);
+                $output['error']=false;
+                echo json_encode($output,JSON_PRETTY_PRINT);
+            }else echo json_encode(array('message'=>"No record found", "error"=>true),JSON_PRETTY_PRINT);  
+            break;
+           
+        case 'search':
+
+            $search = $data['id'];
+
+            //sql query to fetch all records
+            $sql = "SELECT * FROM products WHERE keywords LIKE '%$search%'";
+
+            $result = mysqli_query($conn,$sql) or die('Failed to fetch brand records from DB');
+            if(mysqli_num_rows($result)>0){
+                $output['data'] = mysqli_fetch_all($result,MYSQLI_ASSOC);
+                $output['error']=false;
+                echo json_encode($output,JSON_PRETTY_PRINT);
+            }else echo json_encode(array('message'=>"No record found", "error"=>true),JSON_PRETTY_PRINT);               
+
     }
 
 
